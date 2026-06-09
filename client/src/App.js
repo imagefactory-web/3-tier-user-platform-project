f0fbb85384997b2c628559a05e5318751622516e
Surya Raghava <factoryimage2@gmail.com>
2026-06-09 15:43:52 +0530

    Update branding and enhance UI styles for UserHub platform

--- client/src/App.js
+++ client/src/App.js
@@ -42,16 +42,15 @@ function App() {
     return (
         <div className="app">
             <div className="navbar">
-                <div className="brand">DevOps Shack</div>
+                <div className="brand">UserHub</div>
                 <div className="nav-links">
                     <a className="home" href="/">Home</a>
-                    <a className="youtube" href="https://www.youtube.com/@devopsshack/videos">YouTube</a>
-                    <a className="courses" href="https://www.devopsshack.com/">Courses</a>
-                    <a className="telegram" href="https://t.me/+9roGPjX1YI42Yzdl">Telegram</a>
-                    <a className="instagram" href="https://www.instagram.com/devopsshack/">Instagram</a>
+                    <a className="about" href="#about">About</a>
+                    <a className="features" href="#features">Features</a>
+                    <a className="contact" href="#contact">Contact</a>
                 </div>
             </div>
-            <h1>DevOps Shack User Management App</h1>
+            <h1>UserHub - User Management Platform</h1>
             <div className="form-container">
                 <input type="text" placeholder="New user name" value={name} onChange={(e) => setName(e.target.value)} />
                 <input type="email" placeholder="New user email" value={email} onChange={(e) => setEmail(e.target.value)} />
